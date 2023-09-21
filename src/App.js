6dc6efe49fe0efab8aac9ebc896c790f23480813
Gunna9999 <emmanuelugochukwu2000@gmail.com>
2023-09-21 08:05:08 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/login" element={<Auth />} />
+        <Route path="/" element={<Auth />} />
         <Route path="/Gallery" element={<Gallery />} />
       </Routes>
     </div>
